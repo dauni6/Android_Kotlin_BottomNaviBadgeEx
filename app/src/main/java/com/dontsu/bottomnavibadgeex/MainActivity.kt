1157e969de10f5e92259da28d30d01b3ed25fe5c
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-15 22:27:44 +0900

    attach Badge
    attach Badge

--- app/src/main/java/com/dontsu/bottomnavibadgeex/MainActivity.kt
+++ app/src/main/java/com/dontsu/bottomnavibadgeex/MainActivity.kt
@@ -2,9 +2,16 @@ package com.dontsu.bottomnavibadgeex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.MenuItem
+import android.view.View
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.badge.BadgeUtils
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_badge_layout.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +20,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         bottomNV.itemIconTintList = null //null 해줘야 비로소 selector_.xml 이 작동함
 
+
         bottomNV.setOnNavigationItemSelectedListener { menuItem: MenuItem ->
+
             when(menuItem.itemId) {
                 R.id.action_people -> {
+                    val menuIdx = menuItem.itemId
+                    var badge = bottomNV.getOrCreateBadge(menuIdx)
+                    badge.isVisible = true
+                    badge.number = 56
                     Snackbar.make(mainLayout, "피플뷰", Snackbar.LENGTH_SHORT).show()
+
                     true
                 }
                 R.id.action_chat -> {
@@ -35,4 +49,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 }

49a8df5fbf13a673be23b24b047d94f660d3a2a8
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-15 20:50:37 +0900

    using SnackBar
    using SnackBar

--- app/src/main/java/com/dontsu/bottomnavibadgeex/MainActivity.kt
+++ app/src/main/java/com/dontsu/bottomnavibadgeex/MainActivity.kt
@@ -2,6 +2,8 @@ package com.dontsu.bottomnavibadgeex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottomNV.itemIconTintList = null //null 해줘야 비로소 selector_.xml 이 작동함
+
+        bottomNV.setOnNavigationItemSelectedListener { menuItem: MenuItem ->
+            when(menuItem.itemId) {
+                R.id.action_people -> {
+                    Snackbar.make(mainLayout, "피플뷰", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.action_chat -> {
+                    Snackbar.make(mainLayout, "챗뷰", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.action_search -> {
+                    Snackbar.make(mainLayout, "검색뷰", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+                else -> {
+                    Snackbar.make(mainLayout, "설정뷰", Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+
+            }
+        }
     }
 }
